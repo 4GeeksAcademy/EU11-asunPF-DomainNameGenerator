6db50221de598d55c281408d576ecfe14f80600b
Asun Perea Ferrer <118168204+asun-pf@users.noreply.github.com>
2023-05-30 21:37:02 +0000

    putted the randomized words in an objet inside an arrow, picked the values of the objects with object.values and picking them all with forEach. Randomizing it with forEach.

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+};
+
+const generateExcuse = () => {
+  const excuseObject = {
+    pronoun: ["save", "our"],
+    adj: ["great", "school"],
+    noun: ["jogger", "yoga"],
+    end: [".com"]
+  };
+
+  let finalPhrase = "";
+
+  Object.values(excuseObject).forEach(arr => {
+    const randomizingTheExcuseObjectValues = Math.floor(
+      Math.random() * arr.length
+    );
+    finalPhrase += arr[randomizingTheExcuseObjectValues] + "";
+  });
+
+  return finalPhrase;
 };
